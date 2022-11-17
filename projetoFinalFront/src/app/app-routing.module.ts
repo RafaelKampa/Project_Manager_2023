df633a74ca56d5d55d581abdc2fb205b62296d30
Rafael Kampa <rafael.kampa@mps.com.br>
2022-11-17 17:06:45 -0300

    Bloqueio de telas sem autenticação usando Angular AuthGuard

--- projetoFinalFront/src/app/app-routing.module.ts
+++ projetoFinalFront/src/app/app-routing.module.ts
@@ -4,21 +4,22 @@ import { ClienteComponent } from './cliente/cliente.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AuthguardGuard } from './shared/authguard.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '',
-    redirectTo: '/api/home',
+    redirectTo: '/api/login',
     pathMatch: 'full',
   },
   {
     path: 'api',
     component: LayoutComponent,
     children: [
-      { path: 'home', component: LoginComponent },
-      { path: 'usuario', component: UsuarioComponent },
-      { path: 'cliente', component: ClienteComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'usuario', component: UsuarioComponent, canActivate: [AuthguardGuard] },
+      { path: 'cliente', component: ClienteComponent, canActivate: [AuthguardGuard] },
     ]
   }
 ];
---
+++ projetoFinalFront/src/app/shared/authguard.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthguardService } from './authguard.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthguardGuard implements CanActivate {
+
+  constructor(private authService: AuthguardService){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.isLoggedIn$;
+  }
+  
+}
