4ea6d3e635a7e7728accc49ff0259868e7fd09e5
Rafael Kampa <rafael_kampa@hotmail.com>
2023-06-12 23:39:49 -0300

    Upgrade angular para versão 16.0.5, correções iniciais de layout, implementação de seleção de mês no componente "Financeiro"

--- projetoFinalFront/src/app/financeiro/financeiro.component.ts
+++ projetoFinalFront/src/app/financeiro/financeiro.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import { MatDatepicker } from '@angular/material/datepicker';
+import * as _moment from 'moment';
+import {default as _rollupMoment, Moment} from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-financeiro',
   templateUrl: './financeiro.component.html',
-  styleUrls: ['./financeiro.component.css']
+  styleUrls: ['./financeiro.component.css'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ],
+  encapsulation: ViewEncapsulation.None,
 })
+
 export class FinanceiroComponent implements OnInit {
 
-  constructor() { }
+  date = new FormControl(moment());
+  public buscaSelecionada: string = "";
+  public mesSelecionado: number = 0;
+  public anoSelecionado: number = 0;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  setMonthAndYear(normalizedMonthAndYear: Moment, datepicker: MatDatepicker<Moment>) {
+    const ctrlValue = this.date.value!;
+    ctrlValue.month(normalizedMonthAndYear.month());
+    ctrlValue.year(normalizedMonthAndYear.year());
+    this.date.setValue(ctrlValue);
+    this.mesSelecionado = normalizedMonthAndYear.month() + 1; //normalizedMonthAndYear.month() começa em 0, portanto é necessário adicionar 1
+    this.anoSelecionado = normalizedMonthAndYear.year();
+    console.log(this.mesSelecionado, this.anoSelecionado);
+    datepicker.close();
+  }
 }
